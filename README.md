0146a7fcaf3b5333c9faa28b440a4cbfd7faaf6e
Rene Donner <code@donner.at>
2015-04-19 17:44:58 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+## SystemImageBuilder
+
+[![Build Status](https://travis-ci.org/rened/SystemImageBuilder.jl.png)](https://travis-ci.org/rened/SystemImageBuilder.jl)
+
+`SystemImageBuilder` is a wrapper around the `build_sysimg` utility function. It allows to build a `sys.ji` image of all installed packages, skipping the packages which are on a curated `exclude` list themselves or depend on such an excluded package.
+
+Usage is simple:
+
+```jl
+using SystemImageBuilder
+buildimage()
+```
+
+If you want to first test the image without overwriting the default (recommended), you can specify a targetpath:
+
+```jl
+buildimage(targetpath = "/tmp/mytest/sys")
+```
+
+And then restart Julia with
+
+```
+julia -J /tmp/mytest/sys.ji
+```
+
+In case you did not specify a custom `targetpath`, thus overwriting the default, and want to remove any pre-compiled code, you can run:
+
+```jl
+resetimage()
+```
+
+#### Excluded packages
+
+`SystemImageBuilder` per default excludes a set of packages from the image which are known not to be pre-compilable. Additional packages can be skipped by specifying the `exclude` parameter:
+
+```jl
+buildimage(exclude = [SystemImageBuilder.defaultexclude; "SomePackage"]
+```
---
+++ src/SystemImageBuilder.jl
@@ -0,0 +1,60 @@
+module SystemImageBuilder
+
+using FunctionalData
+
+export buildimage, resetimage
+
+function requires(pkg)
+    dir = Pkg.dir(pkg)
+    filename = @p joinpath dir "REQUIRE"
+    !isfile(filename) && return (pkg,[])
+    reqs = @p Base.Pkg.Reqs.read filename | Base.Pkg.Reqs.parse | keys | collect | filter unequal "julia"
+    pkg, reqs
+end
+
+function recursiverequirements(installed)
+    req = @p map installed requires | Dict
+    makerec(a,k) = @p map a[k] (x->makerec(a,x)) | flatten | vcat a[k] | filter not*isempty | unique | sort
+    map(req, (k,v)->(k,makerec(req,k)))
+end
+
+script = @p joinpath Pkg.dir("SystemImageBuilder") "contrib" "build_sysimg_0.$(VERSION.minor).jl"
+include(script)
+
+defaultexclude = ["Tk","PyPlot","IJulia","SystemImageBuilder"]
+
+
+resetimage() = buildimage(reset = true)
+function buildimage(;exclude = defaultexclude, include = [], targetpath = default_sysimg_path, reset = false)
+    userimg = @p joinpath normpath(Base.JULIA_HOME, "../../base")"userimg.jl"
+
+    if reset
+        try rm(userimg) end
+        println("##  SystemImageBuilder: building clean sys.ji ...\n")
+        build_sysimg(default_sysimg_path, "native")
+    else
+        installed = sort([k for (k,v) in Pkg.installed()])
+        req = recursiverequirements(installed)
+        packages = filter(x->!in(x,exclude), installed)
+        packages = filter(x->isempty(intersect(req[x],exclude)), packages)
+        packages = [include; packages]
+
+        println("##  SystemImageBuilder: the following $(length(packages)) packages will be included in the system image:\n")
+        println(join(packages, ", "))
+
+        skipped = setdiff(installed, packages)
+        if !isempty(skipped)
+            println("\n##  SystemImageBuilder: the following $(length(skipped)) packages will be skipped:\n")
+            println(join(skipped, ", "))
+        end
+        println()
+
+        @p map packages (x->"try Base.require(\"$x\") end") | unlines | write userimg
+        
+        println("##  SystemImageBuilder: invoking build_sysimg ...\n")
+        mkpath(targetpath)
+        build_sysimg(targetpath, "native")
+    end
+end
+
+end # module
